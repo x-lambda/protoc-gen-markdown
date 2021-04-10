fc303953729e3857d4ff7a8095b88a899570096d
x-lambda <ilib0x00000000@aliyun.com>
2021-04-10 10:49:27 +0800

    github package

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,35 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 
-	"github.com/golang/protobuf/proto"
-	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	proto "github.com/x-lambda/protoc-gen-markdown"
 )
 
 func main() {
-	req := readGenRequest(os.Stdin)
+	req := proto.ReadGenRequest(os.Stdin)
 
 	// file_to_generate
 	for _, v := range req.FileToGenerate {
 		fmt.Println(v)
 	}
-}
-
-func readGenRequest(r io.Reader) *plugin.CodeGeneratorRequest {
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "read err: %+v", err)
-		panic(err)
-	}
-
-	req := new(plugin.CodeGeneratorRequest)
-	if err = proto.Unmarshal(data, req); err != nil {
-		fmt.Fprintf(os.Stderr, "proto unmarshal err: %+v", err)
-		panic(err)
-	}
-
-	return req
 }
---
+++ params_test.go
@@ -0,0 +1,14 @@
+package protoc_gen_markdown
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseParams(t *testing.T) {
+	s := "path_prefix=/tmp,package_name=api"
+	p, err := parseParams(s)
+	assert.Nil(t, err)
+	assert.Equal(t, p.PathPrefix, "/tmp")
+}
